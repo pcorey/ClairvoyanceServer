0ab3d12b3dd3faa715622a7a8e1ba87c542eb31a
pcorey <petecorey@gmail.com>
2013-10-30 14:58:00 -0700

    socket testing

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express.createServer();
-var io = require('socket.io').listen(app);
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 
 app.use(express.logger());
 
@@ -9,19 +11,13 @@ app.get('/', function (req, res) {
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+server.listen(port, function() {
    console.log('Listening on ' + port);
 });
 
-/*var express = require("express");
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-     response.send('Hello World!');
+io.sockets.on('connection', function(socket) {
+   socket.on('selection', function(data) {
+      console.log(data);
+   });
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-     console.log("Listening on " + port);
-});*/
