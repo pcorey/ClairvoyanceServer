18757d4f785262405c8690e9c7d425b104261af9
pcorey <petecorey@gmail.com>
2013-10-30 14:36:59 -0700

    Fixed bug

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
    console.log('Listening on ' + port);
-}
+});
 
 /*var express = require("express");
 var app = express();
